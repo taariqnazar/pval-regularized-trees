8d3b2debf333311afd2de06c37046ee8b0590267
Nils Engler <nils.engler@arcor.de>
2023-11-07 11:03:48 +0100

    efficient computation

--- code/chi_sq_dist.py
+++ code/chi_sq_dist.py
@@ -42,6 +42,18 @@ def chi_sq_dist(x, K_acc, df, coffs):
 
 
 def chi_sq_cdf(x, dfs, coffs):
+	if(dfs[0] == dfs[1]):
+		dfs = [dfs[0] + dfs[1], 1]
+		coffs = [coffs[0], coffs[2]]
+		if(coffs[1] - coffs[0] < 0):
+			coffs[0], coffs[1] = coffs[1], coffs[0]
+			dfs[0], dfs[1] = dfs[1], dfs[0]
+		(p1, p2) = (dfs[0]/2) - 1 , (dfs[1]/2) - 1
+		(a1, a2) = coffs[0], coffs[1]
+		const = gamma((sum(dfs))/2)/(gamma(dfs[0]/2)*gamma(dfs[1]/2))
+		f = lambda z: const * ((1-z)**p1)*(z**p2)
+		upper = np.maximum(np.minimum(1, a1/(a1 - a2)), 0)
+		return quad(f, 0, upper)
 	if(coffs[1] - coffs[0] < 0):
 		coffs[0], coffs[1] = coffs[1], coffs[0]
 		dfs[0], dfs[1] = dfs[1], dfs[0]
@@ -51,11 +63,8 @@ def chi_sq_cdf(x, dfs, coffs):
 	f = lambda y, x: const * ((1-x-y)**p1)*(y**p2)*(x**p3)
 	h = lambda x:  (x*(a1 - a3) - a1)/(a2-a1)
 	g = lambda x:  1-x
-	if(a2 - a1 == 0):
-		print('Has to be treated still...')
-	if(a2 - a1 > 0):
-		upper = lambda x:  np.maximum(np.minimum(h(x), g(x)), 0)
-		return  dblquad(f, 0, 1, 0, upper)
+	upper = lambda x:  np.maximum(np.minimum(h(x), g(x)), 0)
+	return  dblquad(f, 0, 1, 0, upper)
 
 def chi_sq_cdf_test(x,dfs, coffs, N):
 	 chi_1 = list(np.random.chisquare(dfs[0],N))
@@ -64,13 +73,32 @@ def chi_sq_cdf_test(x,dfs, coffs, N):
 	 chi_mix = [1 if coffs[0]*chi_1[i] + coffs[1]*chi_2[i] + coffs[2]*chi_3[i] < x else 0 for i in range(N)]
 	 return(sum(chi_mix)/N)
 
+def F(r, n_A, n_B):
+	dfs = [n_A - 1, n_B - 1, 1]
+	coffs = [((n_A + 1)*(n-1) - r*(n+1)*(n_A -1))/(n_A - 1), ((n_B + 1)*(n-1) - r*(n+1)*(n_B -1))/(n_B - 1), - r*(n + 1)]
+	return chi_sq_cdf(0, dfs, coffs)[0]
+
+
+
 
 x = 0
-r = .5
-n_A, n_B = (110, 100)
+r = 1
+n_A, n_B = (50, 50)
 n = n_A + n_B
 dfs = [n_A - 1, n_B - 1, 1]
 coffs = [((n_A + 1)*(n-1) - r*(n+1)*(n_A -1))/(n_A - 1), ((n_B + 1)*(n-1) - r*(n+1)*(n_B -1))/(n_B - 1), - r*(n + 1)]
+
+#print(chi_sq_cdf(x, dfs, coffs))
+
+#print('Simulated:')
+#print(chi_sq_cdf_test(x, dfs, coffs, 1000000))
+
+axis = np.arange(0.8, 1.2, 0.001)
+Fs = [F(r, n_A, n_B) for r in axis]
+plt.plot(axis, Fs)
+plt.show()
+
+
 
 #dfs = [2,3,1]
 #coffs = [-5,-4, 1]
@@ -80,7 +108,7 @@ coffs = [((n_A + 1)*(n-1) - r*(n+1)*(n_A -1))/(n_A - 1), ((n_B + 1)*(n-1) - r*(n
 
 #print(chi_sq_cdf(x, dfs, coffs))
 
-
+'''
 (p1, p2, p3) = (dfs[0]/2) - 1 , (dfs[1]/2) - 1, (dfs[2]/2) - 1
 (a1, a2, a3) = coffs[0], coffs[1], coffs[2]
 print(p1, p2, p3)
@@ -140,10 +168,9 @@ else:
 		print(  dblquad(f, 0, 1, l2, g)[0])
 		print(dblquad(f, 0, 1, l2, g))
 
-print(chi_sq_cdf(x, dfs, coffs))
+'''
 
-print('Simulated:')
-print(chi_sq_cdf_test(x, dfs, coffs, 1000000))
+
 #print(chi_sq_dist(x, K_acc, df, coffs))
 
 
